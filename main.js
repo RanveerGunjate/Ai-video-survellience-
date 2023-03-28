ad4b4fc2e7925169e2c24fab8353c672a817c66a
RanveerGunjate <90454796+RanveerGunjate@users.noreply.github.com>
2023-03-28 16:39:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+video = "";
+
+function preload() {
+    video = createCapture();
+    video.hide();
+}
+function setup()
+{
+    canvas = createCanvas(480, 330);
+    canvas.center();
+}
+function draw() {
+    image(video, 0, 0, 480, 330);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
